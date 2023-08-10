6579457f3cc26a1555ae78aacc350b97b5d0a4cb
rigo1999 <letlovelead271@gmail.com>
2023-08-10 09:46:00 -0700

    printing sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+/**
+ * main - Printing sieze of types in the computer
+ * Return: 0 (for success compilation)
+*/
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+return (0);
+}
