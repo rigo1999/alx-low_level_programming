02bc6e2edeed351e6c47477ad32ae8cd0a69eff4
rigo1999 <letlovelead271@gmail.com>
2023-08-10 08:10:13 -0700

    printng message with puts in the main

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main is the principal function that runs all the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
