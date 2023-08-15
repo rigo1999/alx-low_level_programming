cd3d508ca2cee3db70a1ebdf1ec22c8c36a6cc20
rigo1999 <letlovelead271@gmail.com>
2023-08-15 15:54:56 -0700

    isalpha task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - Iterates the alphabet
+ *
+ * @c: checkes c
+ *
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
+
