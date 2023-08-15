b5a083b334e88129dbc554c749122d9b8ca8a984
rigo1999 <letlovelead271@gmail.com>
2023-08-15 09:21:52 -0700

    task 1 sometimes...

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet - will print the alphabet 
- * followed by a new line
- * Return:void (nothing)
+* print_alphabet - will print the alphabet 
+* followed by a new line
+* Return:void (nothing)
 */
 
 void print_alphabet(void)
 {
 char letter;
-for(letter ='a'; letter <= 'z'; letter++)
+for (letter ='a'; letter <= 'z'; letter++)
 _putchar(letter);
 _putchar('\n');
 }
