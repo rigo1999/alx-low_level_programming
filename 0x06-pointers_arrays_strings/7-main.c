1ba5fa698a7abff1a48371bce32d9d1371b03959
rigo1999 <letlovelead271@gmail.com>
2023-08-24 10:11:51 -0700

    changing values of arr char

--- 0x06-pointers_arrays_strings/7-main.c
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -1,28 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * leet - encodes a string in 1337
- * @s: string to be encoded
+ * main - check the code for
  *
- * Return: the resulting string;
-*/
-char *leet(char *s)
+ * Return: Always 0.
+ */
+int main(void)
 {
-int i, j;
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
 
-char *a = "aAeEoOtTlL";
-char *b = "4433007711";
-
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; j < 10; j++)
-{
-if (s[i] == a[j])
-{
-s[i] = b[j];
-}
-}
-}
-
-return (s);
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
