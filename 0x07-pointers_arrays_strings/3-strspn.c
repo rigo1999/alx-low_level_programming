68004e38b9a92407bcd6bd661ce170b55e5d9bb0
rigo1999 <letlovelead271@gmail.com>
2023-08-28 06:02:14 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function  calculates  the  length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept.
+ * @s: string to search in.
+ * @accept: bytes in which to search for.
+ *
+ * Return: the number of bytes matching accept.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, z, count, checker;
+
+i = 0;
+y = 0;
+checker = 0;
+count = 0;
+while (s[i] != '\0')
+{
+y = 0;
+checker = 0;
+while (accept[y] != '\0')
+{
+if (accept[y] == s[i])
+{
+count++;
+checker = 1;
+/* Break to early increase efficiency */
+break;
+}
+z++;
+}
+
+if (checker == 0)
+return (count);
+i++;
+}
+return (count);
+}
