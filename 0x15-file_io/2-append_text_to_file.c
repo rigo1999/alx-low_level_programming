78d95a939365a8b57f4a75ac51c37bdecf1c876b
root <rigo1999>
2023-10-30 07:05:07 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,28 +4,28 @@
  * append_text_to_file - Appends text at the end of a file.
  * @filename: The name of the file.
  * @text_content: The text to append to the file.
- * Return: 1 on success, -1 if failure.
+ * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_desc, bytes_w, length = 0;
+	int fd, bytes_w, lengh = 0;
 
 	if (filename == NULL)
 		return (-1);
-	file_desc = open(filename, O_WRONLY | O_APPEND);
-	if (file_desc == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[file_desc])
-			file_desc++;
-		bytes_w = write(file_desc, text_content, file_desc);
+		while (text_content[lengh])
+			lengh++;
+		bytes_w = write(fd, text_content, lengh);
 		if (bytes_w == -1)
 		{
-			close(file_desc);
+			close(fd);
 			return (-1);
 		}
 	}
-	close(file_desc);
+	close(fd);
 	return (1);
 }
