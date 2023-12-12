53e9281f7daf4694bcb4018230dc13ed8f979f35
root <rigo1999>
2023-12-12 11:09:57 -0800

    creations of dynamic lib

---
+++ 0x18-dynamic_libraries/the_funcs/100-atoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _atoi - int
+ * @s: pointer
+ * Return: int.
+*/
+
+int _atoi(char *s)
+
+{
+	return (atoi(s));
+}
